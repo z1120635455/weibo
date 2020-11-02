f056f2439d1c57967539a7137b47349bbc73064b
z1120635455 <1120635455@qq.com>
2020-11-02 23:42:33 +0800

    用户注册表单

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@
 Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/signup', 'UsersController@create')->name('signup');
 
-Route::resource('users','UserController');
+
+Route::resource('users','UsersController');
